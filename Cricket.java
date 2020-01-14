27178eebaf988df7cd7c2a660f9f14ef510ddb01
PaviKathirvel <50656931+PaviKathirvel@users.noreply.github.com>
2020-01-14 14:13:22 +0530

    Update Cricket.java

--- Cricket.java
+++ Cricket.java
@@ -5,7 +5,7 @@ public class Cricket{
 	public static void main(String args[]){
 		Scanner scan = new Scanner(System.in);						//scanner to prompt the user for input
 		System.out.print("INPUT : ");
-		String input = scan.nextLine();								//stores the input
+		String input = scan.nextLine();							//stores the input
 		char input_array[] = input.toCharArray();					//converts the input string into character array
 		System.out.println();
 		int score = 0;										
@@ -13,10 +13,10 @@ public class Cricket{
 		int extras = 0;
 		int ball = 0;
 		int wrong_input = 0;
-		int players[] = new int[12];								//array to store the individual's runs
-		int strike = 1;												//player on strike
-		int previous_out = 0;										//player who was the last out
-		int opposite = 2;											//player on opposite strike
+		int players[] = new int[12];							//array to store the individual's runs
+		int strike = 1;									//player on strike
+		int previous_out = 0;								//player who was the last out
+		int opposite = 2;								//player on opposite strike
 		
 		for(int i=0;i<input_array.length;i++){						//traveres through the string or character array
 			
@@ -24,7 +24,7 @@ public class Cricket{
 				int current = strike;
 				strike = opposite;
 				opposite = current;
-			}														//changes the strike after each over
+			}									//changes the strike after each over
 			
 			if(input_array[i] == 'W' || input_array[i] == 'w'){
 				wickets++;
@@ -42,11 +42,11 @@ public class Cricket{
 					previous_out=strike;
 					strike = previous_out+1;
 				}
-			}														//changes the strike after a wicket
+			}									//changes the strike after a wicket
 			else if(input_array[i] == 'E' || input_array[i] == 'e'){
 				extras++;
 				score++;
-			}														//counts for extras
+			}									//counts for extras
 			else if(input_array[i] == '.'){
 				ball++;
 			}														
@@ -64,23 +64,23 @@ public class Cricket{
 					strike = strike;
 					opposite = opposite;
 				}
-			}														//changes the strike after each run and counts the runs for individual player
+			}									//changes the strike after each run and counts the runs for individual player
 			else{
 				System.out.println("Wrong Input");
 				wrong_input = 1;
 				break;
-			}														//when there is a wrong input
+			}									//when there is a wrong input
 			
 		}
 		if(wrong_input != 1){
 			System.out.println("\n\t\tSCORE TABLE\n");
-			System.out.println("Score	:	"+score);
-			System.out.println("Wickets :	"+wickets);
-			System.out.println("Extras  :	"+extras);
-			System.out.println("Balls	:	"+ball);
-			System.out.println("Overs	:	"+ball/6+"."+ball%6);
-			System.out.println("Strike  :	"+strike);
-			System.out.println("Opposite:	"+opposite);
+			System.out.println("Score	: "+score);
+			System.out.println("Wickets	: "+wickets);
+			System.out.println("Extras      : "+extras);
+			System.out.println("Balls	: "+ball);
+			System.out.println("Overs	: "+ball/6+"."+ball%6);
+			System.out.println("Strike      : "+strike);
+			System.out.println("Opposite    : "+opposite);
 			for(int player = 1;player<players.length;player++){
 				System.out.println("player"+player+" :"+players[player]);
 			}
